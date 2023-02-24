cead87629ab132b772f95d51adfe1f61628a614b
igs4ahsan <ahsan@intelligentsoftware.net>
2023-02-24 15:42:07 +0500

    fix: root message changed

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import "./lib/db";
-import express from "express";
+
 import countryRoutes from "./routes/country";
+import express from "express";
 
 const app = express();
 const port = process.env.PORT || 3333;
@@ -10,7 +11,7 @@ app.use(express.raw({ type: "application/vnd.custom-type" }));
 app.use(express.text({ type: "text/html" }));
 
 app.get("/", async (req, res) => {
-  res.json({ message: "Please visit /countries to view all the countries" });
+  res.json({ message: "Hello world" });
 });
 
 app.use("/countries", countryRoutes);
